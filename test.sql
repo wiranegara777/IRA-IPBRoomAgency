ae6ec3e16a3c42c6732f68e19cfc0a86bcceffe3
Muhammad Wiranegara Girinata <muhammadwiranegara777@gmail.com>
2018-03-18 09:16:36 +0700

    update sql

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2018 at 02:48 AM
+-- Generation Time: Mar 17, 2018 at 08:15 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -35,59 +35,18 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `nim` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
-  `departemen` varchar(255) NOT NULL
+  `departemen` varchar(255) NOT NULL,
+  `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `nim`, `phone`, `departemen`) VALUES
-(1, 'wiranegara ', 'muhammadwiranegara777@gmail.com', 'superman777', 'G64150058', '085780263714', 'Ilmu Komputer'),
-(2, 'Furqan', 'furqan@gmail.com', 'qwerty', 'G641500589', '', 'FEM'),
-(3, 'Sultanzz', 'sultan@sultan.com', 'qwerty', 'G6450087', '', 'Ilkom');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user2`
---
-
-CREATE TABLE `user2` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user2`
---
-
-INSERT INTO `user2` (`id`, `name`, `age`, `email`) VALUES
-(1, '1231231', 12, 'qweqweqe@gmail.com');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `age` int(3) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `age`, `email`) VALUES
-(1, 'wiranegara', 17, 'gara@gmail.com'),
-(2, 'sultan20', 22, 'sultan20@gmail.com'),
-(3, '1231231', 12, 'qweqweqe@gmail.com');
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `nim`, `phone`, `departemen`, `level`) VALUES
+(2, 'Furqan', 'furqan@gmail.com', 'qwerty', 'G641500589', '', 'FEM', 2),
+(3, 'Sultanzz', 'sultan@sultan.com', 'qwerty', 'G6450087', '', 'Ilkom', 2),
+(10, 'wira', 'muhammadwiranegara777@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G64150058', '', 'Ilkom', 1);
 
 --
 -- Indexes for dumped tables
@@ -100,18 +59,6 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user2`
---
-ALTER TABLE `user2`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -119,19 +66,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user2`
---
-ALTER TABLE `user2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
