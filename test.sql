6d571893f0b2a1869ef93eba865fa04e90c4f4f3
wiranegara777 <muhammadwiranegara777@gmail.com>
2018-04-20 13:55:58 +0700

    DB baru

--- test.sql
+++ test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2018 at 10:49 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 5.6.34
+-- Generation Time: Apr 20, 2018 at 08:54 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `test`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id_message` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_pj` int(11) NOT NULL,
+  `conversation` text NOT NULL,
+  `dates` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sender` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id_message`, `id_user`, `id_pj`, `conversation`, `dates`, `sender`) VALUES
+(1, 3, 10, 'hey pak wira', '2018-04-11 07:04:36', 1),
+(3, 3, 10, 'hmm', '2018-04-15 11:22:59', 1),
+(4, 3, 9, 'tes', '2018-04-15 11:39:34', 0),
+(5, 3, 10, 'Apa sul?', '2018-04-15 12:18:55', 0),
+(6, 11, 10, 'Assalamu\'alaikum apakah aman?', '2018-04-15 12:22:06', 1),
+(7, 11, 10, 'Disini sudah ada apa saja pak?', '2018-04-15 12:22:25', 1),
+(8, 11, 10, 'Ada macem-macem sih ra', '2018-04-15 12:23:35', 0),
+(9, 3, 10, 'WOY jawab nape', '2018-04-15 21:09:53', 0),
+(10, 3, 10, 'eh ', '2018-04-17 23:21:31', 1),
+(11, 3, 10, 'cups', '2018-04-17 23:23:06', 1),
+(12, 3, 10, 'tes wir', '2018-04-17 23:23:57', 1),
+(13, 3, 10, 'apaan sih lu', '2018-04-17 23:26:01', 1),
+(14, 3, 10, 'berisik jing', '2018-04-17 23:28:27', 0),
+(15, 3, 10, 'berisik jing2', '2018-04-17 23:31:29', 0),
+(16, 3, 10, 'berisik jing3', '2018-04-17 23:32:20', 0),
+(17, 0, 10, 'berisik 4', '2018-04-17 23:34:00', 0),
+(18, 3, 10, 'berisik 4', '2018-04-17 23:34:59', 0),
+(19, 3, 10, 'dih gajelas', '2018-04-17 23:35:45', 1),
+(20, 3, 10, 'biasa dong gausah ngegas', '2018-04-17 23:36:12', 0);
 
 -- --------------------------------------------------------
 
@@ -50,7 +90,30 @@ INSERT INTO `order_room` (`id_order`, `id_room`, `id_user`, `id_pj`, `sum_price`
 (2, 6, 10, 10, 16000, 2, 19, 3, 2018, 1),
 (4, 3, 3, 10, 12000, 2, 2, 4, 2018, 1),
 (5, 1, 3, 10, 4000, 2, 28, 3, 2018, 1),
-(6, 8, 3, 10, 54000, 6, 14, 4, 2018, 2);
+(6, 8, 3, 10, 54000, 6, 14, 4, 2018, 2),
+(7, 9, 11, 10, 3000, 3, 17, 4, 2018, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `id_rating` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_room` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`id_rating`, `id_user`, `id_room`, `rating`, `comment`) VALUES
+(1, 10, 2, 3, 'hhaaa'),
+(2, 10, 2, 3, 'hhaaa');
 
 -- --------------------------------------------------------
 
@@ -120,10 +183,22 @@ INSERT INTO `user` (`id`, `name`, `email`, `password`, `nim`, `phone`, `departem
 --
 
 --
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id_message`);
+
+--
 -- Indexes for table `order_room`
 --
 ALTER TABLE `order_room`
   ADD PRIMARY KEY (`id_order`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id_rating`);
 
 --
 -- Indexes for table `room`
@@ -142,10 +217,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT for table `order_room`
 --
 ALTER TABLE `order_room`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `room`
