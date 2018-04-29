fedbd01c8f45ae99b7177a41963a6436848f28a5
wiranegara777 <muhammadwiranegara777@gmail.com>
2018-04-30 06:30:19 +0700

    update database

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2018 at 08:54 AM
+-- Generation Time: Apr 30, 2018 at 01:29 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -60,7 +60,12 @@ INSERT INTO `message` (`id_message`, `id_user`, `id_pj`, `conversation`, `dates`
 (17, 0, 10, 'berisik 4', '2018-04-17 23:34:00', 0),
 (18, 3, 10, 'berisik 4', '2018-04-17 23:34:59', 0),
 (19, 3, 10, 'dih gajelas', '2018-04-17 23:35:45', 1),
-(20, 3, 10, 'biasa dong gausah ngegas', '2018-04-17 23:36:12', 0);
+(20, 3, 10, 'biasa dong gausah ngegas', '2018-04-17 23:36:12', 0),
+(21, 10, 10, 'oy', '2018-04-20 13:58:01', 0),
+(22, 11, 10, 'HOY', '2018-04-20 14:27:24', 1),
+(23, 11, 10, 'Oy sultan', '2018-04-20 14:27:49', 0),
+(24, 11, 10, 'ape', '2018-04-20 14:27:54', 1),
+(25, 11, 10, 'iya sini aja', '2018-04-20 14:28:04', 0);
 
 -- --------------------------------------------------------
 
@@ -113,7 +118,10 @@ CREATE TABLE `rating` (
 
 INSERT INTO `rating` (`id_rating`, `id_user`, `id_room`, `rating`, `comment`) VALUES
 (1, 10, 2, 3, 'hhaaa'),
-(2, 10, 2, 3, 'hhaaa');
+(2, 10, 2, 3, 'hhaaa'),
+(3, 10, 1, 4, 'hehehe'),
+(4, 10, 1, 4, 'hehehe'),
+(5, 10, 1, 2, 'hehehe');
 
 -- --------------------------------------------------------
 
@@ -130,23 +138,31 @@ CREATE TABLE `room` (
   `image` varchar(255) NOT NULL,
   `lat` float(10,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `image2` varchar(255) NOT NULL,
+  `image3` varchar(255) NOT NULL,
+  `fakultas` varchar(255) NOT NULL,
+  `fasilitas` text NOT NULL,
+  `kapasitas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`id_room`, `id_user`, `title`, `body`, `address`, `image`, `lat`, `lng`, `price`) VALUES
-(1, 10, 'sd', '<p>assahsa</p>\r\n', 'sa', 'img/032120180303pm.png', -6.599825, 106.801331, 2000),
-(2, 10, 'yoshhhh', '<p>asjfhbskjf</p>\r\n', 'djkaszd', 'img/032120180306pm.jpg', -6.600848, 106.803047, 5000),
-(3, 10, 'LoGue', '<p>ini adlah sesuatu yang harus dilupakan olehmu nak ahaha</p>\r\n', 'Bogor Agricultural', 'img/032720180124am.png', -6.596414, 106.799614, 6000),
-(5, 10, 'tes2', '<p>Midtrans are the</p>\r\n', 'Jakarta', 'img/032920180700pm.PNG', -6.596909, 106.797173, 7000),
-(6, 10, 'qwertty', '<p>adasdasd</p>\r\n', 'asdasd', 'img/032920180704pm.jpg', -6.561651, 106.769745, 8000),
-(8, 10, 'sadas', '<p>dasdasd</p>\r\n', 'dasdas', 'img/032920180700pm.PNG', -6.572224, 106.692154, 9000),
-(9, 10, 'ganteng', '<p>ini</p>\r\n', 'lampung', 'img/032920180704pm.jpg', -6.568131, 106.675842, 1000),
-(10, 10, 'dia', '<p>asdasdasdasdxzc</p>\r\n', 'qweq', 'img/032920180708pm.jpg', -6.620312, 106.728714, 1000),
-(11, 10, 'polo', '<p>testsetfgfdg</p>\r\n', 'dimana', 'img/033020180137pm.jpg', -6.597462, 106.803734, 50000);
+INSERT INTO `room` (`id_room`, `id_user`, `title`, `body`, `address`, `image`, `lat`, `lng`, `price`, `image2`, `image3`, `fakultas`, `fasilitas`, `kapasitas`) VALUES
+(1, 10, 'RK.X303', '<p>assahsa</p>\r\n', 'sa', 'img/032120180303pm.png', -6.599825, 106.801331, 2000, 'img/032720180124am.png', 'img/032720180124am.png', 'FMIPA', 'AC,proyektor,Kursi', 200),
+(2, 10, 'Audit Mandiri', '<p>asjfhbskjf</p>\r\n', 'djkaszd', 'img/032120180306pm.jpg', -6.600848, 106.803047, 5000, 'img/032720180124am.png', 'img/032720180124am.png', 'FAHUTAN', 'AC,proyektor,Kursi', 200),
+(3, 10, 'Audit PPKU', '<p>ini adlah sesuatu yang harus dilupakan olehmu nak ahaha</p>\r\n', 'Bogor Agricultural', 'img/032720180124am.png', -6.596414, 106.799614, 6000, 'img/032720180124am.png', 'img/032720180124am.png', 'FAHUTAN', 'AC,proyektor,Kursi', 200),
+(5, 10, 'RK.U201', '<p>Midtrans are the</p>\r\n', 'Jakarta', 'img/032920180700pm.PNG', -6.596909, 106.797173, 7000, 'img/032720180124am.png', 'img/032720180124am.png', 'FATETA', 'AC,proyektor,Kursi', 200),
+(6, 10, 'FAC.207', '<p>adasdasd</p>\r\n', 'asdasd', 'img/032920180704pm.jpg', -6.561651, 106.769745, 8000, 'img/032720180124am.png', 'img/032720180124am.png', 'FMIPA', 'AC,proyektor,Kursi', 200),
+(8, 10, 'LSI', '<p>dasdasd</p>\r\n', 'dasdas', 'img/032920180700pm.PNG', -6.572224, 106.692154, 9000, 'img/032720180124am.png', 'img/032720180124am.png', 'FMIPA', 'AC,proyektor,Kursi', 200),
+(9, 10, 'Rektorat', '<p>ini</p>\r\n', 'lampung', 'img/032920180704pm.jpg', -6.568131, 106.675842, 1000, 'img/032720180124am.png', 'img/032720180124am.png', 'FPIK', 'AC,proyektor,Kursi', 200),
+(10, 10, 'R.Sidang Ilkom IPB', '<p>asdasdasdasdxzc</p>\r\n', 'qweq', 'img/032920180708pm.jpg', -6.620312, 106.728714, 1000, 'img/032720180124am.png', 'img/032720180124am.png', 'PPKU', 'AC,proyektor,Kursi', 200),
+(11, 10, 'Audit AHN', '<p>testsetfgfdg</p>\r\n', 'dimana', 'img/033020180137pm.jpg', -6.597462, 106.803734, 50000, 'img/032720180124am.png', 'img/032720180124am.png', 'UMUM', 'AC,proyektor,Kursi', 200),
+(13, 2, 'RK.X304', '<p>dfgdgg</p>\r\n', 'dasd', 'img/043020180117am.jpg', -6.598461, 106.702110, 1000, 'img/043020180117am.jpg', 'img/043020180117am.jpg', 'dasd', 'sad', 30),
+(14, 2, 'RK.X30455', '<p>dfgdgg</p>\r\n', 'dasd', 'img/043020180122am.jpg', -6.598461, 106.702110, 1000, 'img/043020180122am.jpg', 'img/043020180122am.jpg', 'dasd', 'sad', 30),
+(15, 2, 'RKRKRKR', '<p>dfgdgg</p>\r\n', 'dasd', 'img/043020180127am.jpg', -6.639385, 106.561005, 1000, 'img/social_share_room.jpg', 'img/DeluxeSuite_FINAL_large.jpg', 'dasd', 'sad', 30);
 
 -- --------------------------------------------------------
 
@@ -162,21 +178,24 @@ CREATE TABLE `user` (
   `nim` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `departemen` varchar(255) NOT NULL,
-  `level` int(11) NOT NULL
+  `level` int(11) NOT NULL,
+  `fakultas` varchar(255) NOT NULL,
+  `angkatan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `nim`, `phone`, `departemen`, `level`) VALUES
-(2, 'Furqan', 'furqan@gmail.com', 'qwerty', 'G641500589', '', 'FEM', 2),
-(3, 'Sultanzz', 'sultan@sultan.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G6450087', '', 'Ilkom', 2),
-(10, 'wira', 'muhammadwiranegara777@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G64150058', '', 'Ilkom', 1),
-(11, 'ira', 'ipbroomagency@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'g641', '', 'G6', 2),
-(12, 'Wira ganteng', 'haha@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', '0812', '', 3),
-(13, 'Wira ganteng 2', 'haha2@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', '0812', '', 3),
-(21, 'weeaw', 'aa@ddd.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', '123456', '', 3);
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `nim`, `phone`, `departemen`, `level`, `fakultas`, `angkatan`) VALUES
+(2, 'Furqan', 'furqan@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G641500589', '', 'FEM', 1, '', ''),
+(3, 'Sultanzz', 'sultan@sultan.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G6450087', '', 'Ilkom', 2, '', ''),
+(10, 'wira', 'muhammadwiranegara777@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G64150058', '', 'Ilkom', 1, '', ''),
+(11, 'ira', 'ipbroomagency@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'g641', '', 'G6', 2, '', ''),
+(12, 'Wira ganteng', 'haha@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', '0812', '', 3, '', ''),
+(13, 'Wira ganteng 2', 'haha2@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', '0812', '', 3, '', ''),
+(21, 'weeaw', 'aa@ddd.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', '', '123456', '', 3, '', ''),
+(24, 'Muhammad Wiranegara Girinata', 'wiranegara_777@apps.ipb.ac.id', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'G64150058', '35235234623', 'Ilmu Komputer', 2, 'FMIPA', '2015');
 
 --
 -- Indexes for dumped tables
@@ -220,7 +239,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `order_room`
@@ -232,19 +251,19 @@ ALTER TABLE `order_room`
 -- AUTO_INCREMENT for table `rating`
 --
 ALTER TABLE `rating`
-  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `id_room` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_room` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
